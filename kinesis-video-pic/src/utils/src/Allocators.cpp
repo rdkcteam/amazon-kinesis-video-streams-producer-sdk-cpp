353976b974a88873852d6b715b655f21f2b616ae
Fnu Kumar Gaurav <FnuKumar_Gaurav@cable.comcast.com>
2020-04-27 13:21:54 -0400

    master upstream with aws sdk

--- kinesis-video-pic/src/utils/src/Allocators.cpp
+++
@@ -1,6 +0,0 @@
-#include "Include_i.h"
-
-memAlloc globalMemAlloc = defaultMemAlloc;
-memAlignAlloc globalMemAlignAlloc = defaultMemAlignAlloc;
-memCalloc globalMemCalloc = defaultMemCalloc;
-memFree globalMemFree = defaultMemFree;
